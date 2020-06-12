fa52317cb668334c37e072695b347af31b07078f
Jason <top1236951@gmail.com>
2020-06-12 00:52:11 -0700

    Revert "Revert "Revert "initial set up for teams page"""
    This reverts commit 7bd9b59899cc7aa80c3d2776b73445dbc16772f0.

--- bio/models.py
+++ bio/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Bio(models.Model):
-    name = models.CharField(max_length=255)
-    role = models.CharField(max_length=255)
-    bio = models.TextField()
-    headshot = models.CharField(max_length=255)
-    board = models.BooleanField(default=False)
